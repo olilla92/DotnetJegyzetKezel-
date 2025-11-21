402d964744d887bb4977110e149613ecc4270453
olilla92 <c0ocslil@vasvari.org>
2025-11-21 14:52:42 +0100

    Elkészült a két model.

---
+++ backend/Models/Notes.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApp.Backend.Models
+{
+    public class Notes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content {  get; set; } = string.Empty;
+        [Required]
+        public bool IsPublic { get; set; } = false;
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey(nameof(Users))]
+        public int UserId { get; set; }
+        public Users Users { get; set; } = null!;
+
+    }
+}
---
+++ backend/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Backend.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required, MaxLength(100), EmailAddress]
+        public string Email { get; set; } = string.Empty ;
+
+        [Required, MaxLength(30)]
+        public string Password { get; set; } = string.Empty;
+
+        public virtual ICollection<Notes> Notes { get; set; } = new List<Notes>();
+    }
+}
